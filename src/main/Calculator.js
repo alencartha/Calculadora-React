abf2c67a7a46e71f8dab3f2c5656612ae340e319
alencartha <alencar_tha@hotmail.com>
2021-02-08 21:46:19 -0300

    implementação da lógica

--- src/main/Calculator.js
+++ src/main/Calculator.js
@@ -1,47 +1,75 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import './Calculator.css'
 import Button from '../components/Button.js'
 import Display from '../components/Display.js'
+
+const initialState = {
+    displayValue:'0',
+    clearDisplay:'false',
+    operation:'null',
+    values:[0, 0],
+    current:0
+}
 
 
 
 export default class Calculator extends Component {
 
-    clearMemory(){
-        console.log('limpar')
+    state = { ...initialState }
+
+    clearMemory() {
+        this.setState({ ...initialState })
     }
 
-    setOperation(operation){
+    setOperation(operation) {
         console.log(operation)
     }
 
-    addDigit(n){
-        console.log(n)
+    addDigit(number) {
+
+        if (number === '.' && this.state.displayValue.includes('.')) {
+            return
+        }
+
+        const clearDisplay = this.state.displayValue === '0'
+            || this.state.clearDisplay
+        const currentValue = clearDisplay ? '' : this.state.displayValue
+        const displayValue = currentValue + number
+        this.setState({displayValue, clearDisplay: false})
+
+        if(number!=='.'){
+            const indice = this.state.current
+            const newValue = parseFloat(displayValue)
+            const values = [...this.state.values]
+            values[indice] = newValue
+            this.setState({values})
+            console.log(values)
+        }
     }
 
-    render(){
-        const addDigit = n => this.addDigit(n)
+    render() {
+        const addDigit = number => this.addDigit(number)
         const setOperation = operation => this.setOperation(operation)
-        return(
+        return (
             <div className="calculator">
-                <Display value={0}/>
-                <Button label="AC" click={()=> this.clearMemory()} triple />   
-                <Button label="/" click={setOperation} operation/>  
-                <Button label="7" click={addDigit} />  
-                <Button label="8" click={addDigit}/> 
-                <Button label="9" click={addDigit}/> 
-                <Button label="*" click={setOperation} operation/> 
-                <Button label="4" click={addDigit}/> 
-                <Button label="5" click={addDigit}/> 
-                <Button label="6" click={addDigit}/> 
-                <Button label="-" click={setOperation} operation /> 
-                <Button label="1" click={addDigit}/> 
-                <Button label="2" click={addDigit}/> 
-                <Button label="3" click={addDigit}/> 
-                <Button label="+" click={setOperation} operation/> 
-                <Button label="0" click={addDigit} double/> 
-                <Button label="." click={setOperation}/> 
-                <Button label="=" click={setOperation} operation/> 
+                <Display value={this.state.displayValue} />
+                <Button label="AC" click={() => this.clearMemory()} triple />
+                <Button label="/" click={setOperation} operation />
+                <Button label="7" click={addDigit} />
+                <Button label="8" click={addDigit} />
+                <Button label="9" click={addDigit} />
+                <Button label="*" click={setOperation} operation />
+                <Button label="4" click={addDigit} />
+                <Button label="5" click={addDigit} />
+                <Button label="6" click={addDigit} />
+                <Button label="-" click={setOperation} operation />
+                <Button label="1" click={addDigit} />
+                <Button label="2" click={addDigit} />
+                <Button label="3" click={addDigit} />
+                <Button label="+" click={setOperation} operation />
+                <Button label="0" click={addDigit} double />
+                <Button label="." click={addDigit} />
+                <Button label="=" click={setOperation} operation />
             </div>
         )
     }
